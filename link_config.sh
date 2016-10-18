e266fcc2a066c4acfda61ada813f57d2e817fa10
nickvanderstel <njvander@gmail.com>
2016-10-17 21:47:44 -0400

    added bashrc link

--- link_config.sh
+++ link_config.sh
@@ -2,6 +2,7 @@
 
 mkdir -p $HOME/.config
 
+ln -sf $(pwd)/bash/.bashrc $HOME/.bashrc
 ln -sf $(pwd)/git/.gitconfig $HOME/.gitconfig
 ln -sf $(pwd)/nvim $HOME/.config/nvim
 ln -sf $(pwd)/script_common $HOME/.config/script_common
