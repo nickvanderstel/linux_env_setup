c68397510348fe19cc077df928db688422eb2361
nickvanderstel <njvander@gmail.com>
2016-10-19 12:44:06 -0400

    added git installer

--- script_common/install.sh
+++ script_common/install.sh
@@ -4,12 +4,24 @@
 determine_os
 
 
+install_git () {
+    ensure_root_access
+
+    echo "Installing git"
+    case "$OS_RELEASE_ID" in
+        "fedora")
+            $SUDO_CMD dnf -y install git
+            ;;
+    esac
+}
+
+
 install_python () {
     ensure_root_access
 
+    echo "Installing python"
     case "$OS_RELEASE_ID" in
         "fedora")
-            echo "Installing python"
             $SUDO_CMD dnf -y install python python3 python-pip python3-pip
             ;;
     esac
@@ -19,9 +31,9 @@ install_python () {
 install_neovim () {
     ensure_root_access
 
+    echo "Installing neovim"
     case "$OS_RELEASE_ID" in
         "fedora")
-            echo "Installing neovim"
             $SUDO_CMD dnf -y install dnf-plugins-core
             $SUDO_CMD dnf -y install copr enable dperson/neovim
             $SUDO_CMD dnf -y install neovim
