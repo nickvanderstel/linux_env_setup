0e8525b6d17a3e99d26c6f74f0f7411ca357910c
nickvanderstel <njvander@gmail.com>
2016-10-19 13:20:58 -0400

    fix function args

--- install_tools.sh
+++ install_tools.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
+# make sure links are set up
+. ./link_config.sh
+
+
 . $HOME/.config/script_common/util.sh
 determine_os
 
 . $HOME/.config/script_common/install.sh
 
 
-# make sure links are set up
-. ./link_config.sh
-
-
-if !is_installed(git); then
+if !is_installed("git"); then
     install_git
 fi
 
 
-if !is_installed(python3); then
+if !is_installed("python3"); then
     install_python
 fi
 
 
-if !is_installed(nvim); then
+if !is_installed("nvim"); then
     install_neovim
 fi
